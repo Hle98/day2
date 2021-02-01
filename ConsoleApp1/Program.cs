17759520bb891bb53d2246885c171ae8c56bbf41
Huy Le <Lequochuy110898@gmail.com>
2021-02-01 17:08:50 -0500

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            double a, b, c, d,x1,x2,x;
+            Console.Write("a = ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b = ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("c = ");
+            c = Convert.ToDouble(Console.ReadLine());
+            d = b*b - 4*a*c;
+           
+            if (d > 0)
+            {
+                x1 = (-b + Math.Sqrt(d)) / 2*a;
+                x2 = (-b - Math.Sqrt(d)) / 2*a;
+                Console.WriteLine($"The equation has 2 solutions: {x1} and {x2}");
+            }
+            else if (d == 0)
+            {
+                x = -b / 2 * a;
+                Console.WriteLine($"The equation has one solution: {x}");
+            }
+            else
+            { 
+                
+            }
+
+            
+        }
+    }
+}
